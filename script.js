fc3e2def1f6657877f1782cfff15aa3f0b20c143
Joshua Foate <joshuafoate@gmail.com>
2024-03-10 21:28:40 -0500

    Fully functional todo list is ready.
    Need to do's: Add styles to make it look pretty, keep the information from being reset when page is reset.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let todos = [];
+
 //Function to create the initial to-do page
 function initialToDoPage () {
     createToDoList();
@@ -30,7 +32,7 @@ function createToDoList () {
 function toDoListTitle() {
     const title = document.createElement('input');
     title.classList.add('to-do-list-title');
-    title.value = 'To-do List';
+    title.value = 'Todo List';
     return title;
 }
 
@@ -41,7 +43,8 @@ function addTaskButton() {
     taskButton.textContent = '+ Add task';
     taskButton.addEventListener('click', function() {
         const toDoList = taskButton.parentNode;
-        addTask(toDoList, taskButton);
+        taskButton.style.display = 'none';
+        createTaskForm(toDoList, taskButton);
     })
     return taskButton;
 }
@@ -52,24 +55,102 @@ function addListButton() {
     toDoListButton.classList.add('to-do-list-button');
     toDoListButton.textContent = '+ Add list';
     toDoListButton.addEventListener('click', function() {
+        toDoListButton.remove();
         createToDoList();
     })
     return toDoListButton;
 }
 
 // The logic behind the add task button
-function addTask(toDoListContainer, thisTaskButton) {
+function addTask(toDoListContainer, thisTaskButton, title, dueDate, priority, description) {
     const container = document.createElement('div');
     container.classList.add('task-container');
 
-    const newTask = document.createElement('input');
-    newTask.classList.add('task');
     const checkBox = document.createElement('input');
     checkBox.setAttribute("type", "checkbox");
 
+    const newTitle = document.createElement('input');
+    newTitle.classList.add('task-title');
+    newTitle.value = title;
+
+    const newDate = document.createElement('input');
+    newDate.classList.add('task-date');
+    newDate.value = dueDate;
+
+    const newPriority = document.createElement('input');
+    newPriority.classList.add('task-priority');
+    newPriority.value = priority;
+
+    const newDescription = document.createElement('input');
+    newDescription.classList.add('task-description');
+    newDescription.value = description;
+
     container.appendChild(checkBox);
-    container.appendChild(newTask);
+    container.appendChild(newTitle);
+    container.appendChild(newDate);
+    container.appendChild(newPriority);
+    container.appendChild(newDescription);
+    thisTaskButton.style.display = 'block';
     toDoListContainer.insertBefore(container, thisTaskButton);
+
+    const todo = createToDo(title, description, dueDate, priority);
+    todos.push(todo);
+}
+
+// Function to create a form for task information
+function createTaskForm(toDoListContainer, thisTaskButton) {
+    const taskForm = document.createElement('form');
+    taskForm.innerHTML = `
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title"><br>
+        <label for="due-date">Due Date:</label>
+        <input type="date" id="due-date" name="due-date"><br>
+        <label for="priority">Priority:</label>
+        <select id="priority" name="priority">
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+        </select><br>
+        <label for="description">Description:</label>
+        <input type="text" id="description" name="description"><br>
+        <button type="submit" id="form-button">
+            <img src="./imgs/send.png" id="send-img">
+        </button
+    `;
+    toDoListContainer.insertBefore(taskForm, thisTaskButton);
+
+    const submitButton = document.querySelector('#form-button');
+    const titleInput = document.querySelector('#title');
+    const submitImg = document.querySelector('#send-img');
+    submitButton.disabled = true;
+    submitImg.style.opacity = 0.5;
+
+    titleInput.addEventListener('input', function() {
+        if (titleInput.value.trim() !== '') {
+            submitButton.disabled = false;
+            submitImg.style.opacity = 1.0;
+        }
+        else {
+            submitButton.disabled = true;
+            submitImg.style.opacity = 0.5;
+        }
+    });
+
+    taskForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        taskFormSubmitButton(toDoListContainer, thisTaskButton, taskForm);
+    });
+}
+
+// Logic behind task-form submit button
+function taskFormSubmitButton(toDoListContainer, thisTaskButton, thisTaskForm) {
+    const formData = new FormData(thisTaskForm);
+    const title = formData.get("title");
+    const dueDate = formData.get("due-date");
+    const priority = formData.get("priority");
+    const description = formData.get("description");
+    addTask(toDoListContainer, thisTaskButton, title, dueDate, priority, description);
+    thisTaskForm.remove();
 }
 
 //
